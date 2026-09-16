1241ca29af0354e39c063d7acda9004e6fbef6cc
Sergio <sergiotarxz@posteo.net>
2026-09-17 00:37:30 +0200

    Allowing full build.

--- complete_build.sh
+++ complete_build.sh
@@ -9,18 +9,18 @@ suffix=$1
 if [ -z $suffix ]; then
     suffix="$(date -u +%Y%m%d%H%M%S)"
 fi
-#echo generating stage1
-#if [ ! -e "/var/tmp/algabuild/stages/stage1-algaos-$suffix.tar.xz" ]; then 
-#    sleep 5;
-#    sudo perl scripts/build.pl --spec stage1-algaos.spec --repo ../algaos-ebuild-tree/ --suffix $suffix -j8 --binpkg-dir /var/tmp/algabuild/stages/binpkg-algaos-latest/ || exit 1
-#    sudo cp /var/tmp/algabuild/stages/stage1-algaos-$suffix.tar.xz /var/tmp/algabuild/stages/stage1-algaos-latest.tar.xz
-#fi
-#echo generating stage2
-#if [ ! -e "/var/tmp/algabuild/stages/stage2-algaos-$suffix.tar.xz" ]; then 
-#    sleep 5;
-#    sudo perl scripts/build.pl --spec stage2-algaos.spec --repo ../algaos-ebuild-tree/ --suffix $suffix -j8 --binpkg-dir /var/tmp/algabuild/stages/binpkg-algaos-latest/ || exit 1
-#    sudo cp /var/tmp/algabuild/stages/stage2-algaos-$suffix.tar.xz /var/tmp/algabuild/stages/stage2-algaos-latest.tar.xz
-#fi
+echo generating stage1
+if [ ! -e "/var/tmp/algabuild/stages/stage1-algaos-$suffix.tar.xz" ]; then 
+    sleep 5;
+    sudo perl scripts/build.pl --spec stage1-algaos.spec --repo ../algaos-ebuild-tree/ --suffix $suffix -j8 --binpkg-dir /var/tmp/algabuild/stages/binpkg-algaos-latest/ || exit 1
+    sudo cp /var/tmp/algabuild/stages/stage1-algaos-$suffix.tar.xz /var/tmp/algabuild/stages/stage1-algaos-latest.tar.xz
+fi
+echo generating stage2
+if [ ! -e "/var/tmp/algabuild/stages/stage2-algaos-$suffix.tar.xz" ]; then 
+    sleep 5;
+    sudo perl scripts/build.pl --spec stage2-algaos.spec --repo ../algaos-ebuild-tree/ --suffix $suffix -j8 --binpkg-dir /var/tmp/algabuild/stages/binpkg-algaos-latest/ || exit 1
+    sudo cp /var/tmp/algabuild/stages/stage2-algaos-$suffix.tar.xz /var/tmp/algabuild/stages/stage2-algaos-latest.tar.xz
+fi
 echo generating stage3
 if [ ! -e "/var/tmp/algabuild/stages/stage3-algaos-$suffix.tar.xz" ]; then 
     sleep 5;
